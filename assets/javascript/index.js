e168bb09fa0b76893cbe2118ab61d00bc7f643d7
TyPhyter <tylerjenkins08@gmail.com>
2018-07-05 18:46:54 -0500

    lots of formatting, added recent projects

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,10 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var $navbar = $(".navbar");
     var $navBrand = $(".navbar-brand");
     var $sidebar = $("#sidebar");
     var $sidebarIcon = $("#sidebar-icon");
+    const $about = $('#main-section');
+    const $navBarBtn = $('button.navbar-toggle');
+    const $messageSent = $('#messageSent');
+    $messageSent.hide();
+    const $messageError = $('#messageError');
+    $messageError.hide();
     var sidebarShownOnce = false;
-    var sticky = $navbar.offset().top + 20;
+    var sticky = $navbar.offset().top;
     var glitchTimer;
 
     var path = window.location.pathname;
@@ -20,12 +26,12 @@ $(document).ready(function() {
         } else {
             randomInt = Math.random() * 5000;
         }
-        console.log(randomInt);
+        // console.log(randomInt);
         if ($element) {
             $element.toggleClass("glitch-active");
             clearInterval(glitchTimer);
             glitchTimer = setTimeout(function () {
-                console.log("new interval");
+                // console.log("new interval");
                 autoGlitch($element);
             }, randomInt);
         }
@@ -41,94 +47,102 @@ $(document).ready(function() {
         autoGlitch($navBrand);
     }, 3000);
 
-    $(document).ready(function () {
-        $(window).on('scroll', function () {
-            //change styling on scroll offset
-            if (Math.round($(window).scrollTop()) >= sticky) {
-                $navbar.show();
-                $navbar.addClass("scrolled sticky");
-                if (!sidebarShownOnce) {
-                    $sidebar.addClass("showSidebar");
-                    $sidebarIcon.addClass("rotateIcon");
-                    sidebarShownOnce = true;
-                }
-            } else {
-                if (page === "index.html") {
-                    $navbar.hide();
-                }
-                $navbar.removeClass("scrolled sticky");
-                if (Math.round($(window).scrollTop()) < sticky) {
-                    $sidebar.removeClass("showSidebar");
-                    $sidebarIcon.removeClass("rotateIcon");
-                }
+
+    $(window).on('scroll', function () {
+        //change styling on scroll offset
+        if (Math.round($(window).scrollTop()) >= sticky) {
+            $navbar.show();
+            $navbar.addClass("scrolled sticky");
+            if (!sidebarShownOnce) {
+                $sidebar.addClass("showSidebar");
+                $sidebarIcon.addClass("rotateIcon");
+                sidebarShownOnce = true;
             }
-        });
-
-        $('#about-button').on('click', function (evt) {
-            evt.preventDefault();
-            $about = $('#main-section');
-            TweenMax.to(window, 1, { scrollTo: { y: $about.offset().top + 25 }, ease: Power2.easeInOut });
-
-        });
-
-        $('#skills-button').on('click', function (evt) {
-            evt.preventDefault();
-            $skills = $('#skills');
-            TweenMax.to(window, 1, { scrollTo: { y: $skills.offset().top }, ease: Power2.easeInOut });
-
-        });
-
-        $('#portfolio-button').on('click', function (evt) {
-            evt.preventDefault();
-            $work = $('#work');
-            TweenMax.to(window, 1, { scrollTo: { y: $work.offset().top  }, ease: Power2.easeInOut });
-        });
-
-        $('#contact-button').on('click', function (evt) {
-            evt.preventDefault();
-            $contact = $('#contact');
-            TweenMax.to(window, 1, { scrollTo: { y: $contact.offset().top  }, ease: Power2.easeInOut });
-
-        });
-
-        $('#send-email-btn').on('click', function (evt) {
-            evt.preventDefault();
-            let recipient = "tyler@typhyter.com";
-            let subject = $('input[name="name"]').val().trim() + " " + $('input[name="mail"]').val().trim();
-            let body = $('textarea[name="comment"]').val().trim();
-            $.post('https://frozen-eyrie-20216.herokuapp.com/email', {
-                recipient,
-                subject,
-                body
-            }, function (err, response) {
-                if(err){
-                    console.log(err);
-                }
-                console.log(response);
-            });
-        });
-
-        $sidebarIcon.on("click", function (evt) {
-            $sidebarIcon.toggleClass("rotateIcon");
-            $sidebar.toggleClass("showSidebar");
-        });
-
-        //TweenMax.to("#main-section", 0, { rotationY: -90, transformOrigin: "50% 50% -400px" });
-        //TweenMax.to("#last-line", 0, { rotationY: -90, transformOrigin: "50% 50% -400px" });
-        //TweenMax.to("#my-face", 0, { rotationY: -90 });
-        //TweenMax.to("#about-me", 0, { rotationY: 90, transformOrigin: "50% 50% -400px" });
-        var nameTimeLine = new TimelineMax();
-        nameTimeLine.to("#SVGnameText", 2, { attr: { x: 10 }, ease: SteppedEase.config(12) });
-        nameTimeLine.to("#SVGnameText", 1, { attr: { y: 75 }, ease: Power2.easeOut });
-        nameTimeLine.to("#SVGtagLine1", 2, { attr: { x: 10 }, ease: SteppedEase.config(24) });
-        nameTimeLine.to("#SVGtagLine1", .75, { attr: { y: 60 }, delay: 0.1, ease: Power2.easeOut });
-        nameTimeLine.to(window, 1, { scrollTo: { y: sticky+5 }, delay: 0.75, ease: Power2.easeInOut });
-        nameTimeLine.to("#my-face", 0, { rotationY: 0 });
-        nameTimeLine.to("#about-me", 0, { rotationY: -0, transformOrigin: "50% 50% -400px" });
-        //nameTimeLine.to("#main-section", 1, { rotationY: 0, transformOrigin: "50% 50% -400px", ease: Power2.easeInOut });
-        //nameTimeLine.to("#last-line", 1, { rotationY: 0, transformOrigin: "50% 50% -400px", ease: Power2.easeInOut });
-
+        } else {
+            if (page === "index.html") {
+                $navbar.hide();
+            }
+            $navbar.removeClass("scrolled sticky");
+            if (Math.round($(window).scrollTop()) < sticky) {
+                $sidebar.removeClass("showSidebar");
+                $sidebarIcon.removeClass("rotateIcon");
+            }
+        }
     });
+
+    $('#about-button').on('click', function (evt) {
+        evt.preventDefault();
+        TweenMax.to(window, 1, { scrollTo: { y: $about.offset().top }, ease: Power2.easeInOut });
+        $navBarBtn.click();
+    });
+
+    $('#skills-button').on('click', function (evt) {
+        evt.preventDefault();
+        $skills = $('#skills');
+        TweenMax.to(window, 1, { scrollTo: { y: $skills.offset().top }, ease: Power2.easeInOut });
+        $navBarBtn.click();
+    });
+
+    $('#portfolio-button').on('click', function (evt) {
+        evt.preventDefault();
+        $work = $('#work');
+        TweenMax.to(window, 1, { scrollTo: { y: $work.offset().top }, ease: Power2.easeInOut });
+        $navBarBtn.click();
+    });
+
+    $('#contact-button').on('click', function (evt) {
+        evt.preventDefault();
+        $contact = $('#contact');
+        TweenMax.to(window, 1, { scrollTo: { y: $contact.offset().top }, ease: Power2.easeInOut });
+        $navBarBtn.click();
+    });
+
+    $('#send-email-btn').on('click', function (evt) {
+        // evt.preventDefault();
+        // let recipient = "tyler@typhyter.com";
+        // let subject = $('input[name="name"]').val().trim() + " " + $('input[name="mail"]').val().trim();
+        // let body = $('textarea[name="comment"]').val().trim();
+        // $.post('https://frozen-eyrie-20216.herokuapp.com/email', {
+        //     recipient,
+        //     subject,
+        //     body
+        // }, function (err, response) {
+        //     if (err) {
+        //         $messageError.show();
+        //         console.log(err);
+        //         return;
+        //     }
+        //     $messageSent.show();
+        //     console.log(response);
+        // });
+    });
+
+    $sidebarIcon.on("click", function (evt) {
+        $sidebarIcon.toggleClass("rotateIcon");
+        $sidebar.toggleClass("showSidebar");
+    });
+
+    //TweenMax.to("#main-section", 0, { rotationY: -90, transformOrigin: "50% 50% -400px" });
+    //TweenMax.to("#last-line", 0, { rotationY: -90, transformOrigin: "50% 50% -400px" });
+    //TweenMax.to("#my-face", 0, { rotationY: -90 });
+    //TweenMax.to("#about-me", 0, { rotationY: 90, transformOrigin: "50% 50% -400px" });
+    var nameTimeLine = new TimelineMax();
+    nameTimeLine.to("#SVGnameText", 2, { attr: { x: 10 }, ease: SteppedEase.config(12) });
+    nameTimeLine.to("#SVGnameText", 1, { attr: { y: 75 }, ease: Power2.easeOut });
+    nameTimeLine.to("#SVGtagLine1", 2, { attr: { x: 10 }, ease: SteppedEase.config(24) });
+    nameTimeLine.to("#SVGtagLine1", .75, { attr: { y: 60 }, delay: 0.1, ease: Power2.easeOut });
+    setTimeout(() => {
+        if ($(window).scrollTop() < 250) {
+            nameTimeLine.to(window, 1, { scrollTo: { y: $about.offset().top }, delay: 0.75, ease: Power2.easeInOut });
+        }
+    }, 5000);
+
+    // nameTimeLine.to("#my-face", 0, { rotationY: 0 });
+    // nameTimeLine.to("#about-me", 0, { rotationY: -0, transformOrigin: "50% 50% -400px" });
+    //nameTimeLine.to("#main-section", 1, { rotationY: 0, transformOrigin: "50% 50% -400px", ease: Power2.easeInOut });
+    //nameTimeLine.to("#last-line", 1, { rotationY: 0, transformOrigin: "50% 50% -400px", ease: Power2.easeInOut });
+
+
 
     $(".flip3d-card").flip({
         "trigger": "click",
@@ -138,6 +152,6 @@ $(document).ready(function() {
 
     $('.nav-item.disabled').on('click', function (evt) {
         evt.preventDefault();
-    })
+    });
 
 });
